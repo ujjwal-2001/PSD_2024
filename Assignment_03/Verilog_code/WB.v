6b708e20e23d41777354c588c488eddb400806d8
ujjwal-2001 <149869490+ujjwal-2001@users.noreply.github.com>
2024-10-26 15:56:33 +0530

    Update WB.v

--- Assignment_03/Verilog_code/WB.v
+++ Assignment_03/Verilog_code/WB.v
@@ -10,3 +10,15 @@
 //--------------------------------------------DESCRIPTION---------------------------------------------//
 // 
 //----------------------------------------------------------------------------------------------------//
+
+module WB(
+    input wire [1:0] MemtoReg_MEM,
+    input wire [31:0] ALUResult_MEM,
+    input wire [31:0] ReadData,
+    input wire [31:0] Immediate_MEM,
+    output wire [31:0] WriteData
+    );
+
+    assign WriteData = (MemtoReg_MEM[1])? Immediate_MEM : (MemtoReg_MEM[0])? ReadData : ALUResult_MEM;
+
+endmodule
